be16da4915bc4ab9ee7fc4f52cf13340e91548b3
niskuldeep <kuldeepnishad.singh@gmail.com>
2022-04-25 21:29:01 +0530

    user and master account connected

--- Controllers/masterAccount.js
+++ Controllers/masterAccount.js
@@ -1,9 +1,24 @@
 const masterAccountModel = require("../Models/masterAccount");
-const user = require("../Models/user");
+const userModel = require("../Models/user");
 
 module.exports.addMasterAccount = async (req, res) => {
   try {
-    const accountDetails = await masterAccountModel.insertOne(req.body);
+    const accountDetails = await masterAccountModel.create(req.body);
+    const userInfo = {
+      accId: accountDetails._id,
+      firstName: req.body.user.firstName,
+      middleName: req.body.user.middleName,
+      lastName: req.body.user.lastName,
+      age: req.body.user.age,
+      email: req.body.user.email,
+      address: req.body.user.address,
+      gender: req.body.user.gender,
+      type: req.body.user.type,
+      createdAt: req.body.user.createdAt,
+      updatedAt: req.body.user.updatedAt,
+    };
+
+    await userModel.create(userInfo);
     res.status(200).json({
       status: "added",
       data: accountDetails,
